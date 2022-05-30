70ca76db676dff2e54cc397322c4351889d3147b
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-05-31 01:20:07 +0300

    Create 8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+me=str[39:66]
+str = me + str[106:112] + str[:6]
+str=
+print(str)
