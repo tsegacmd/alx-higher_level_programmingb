840f453b405de124c2b0aefaab6b58250fde9719
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-06-01 20:02:55 +0300

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx pro
+ * for alx
  */
 typedef struct listint_s
 {
@@ -17,7 +17,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
